df400102564a1aacc45393e1843996ccb2ce0991
DOBOB_LAPTOP <dlehgus1110@naver.com>
2022-12-16 17:42:00 +0900

    refactor: 동전 리스트 자료구조 Map으로 변경
    리스트 순서를 보장받기 위한 변경

--- src/utils/generateCoinList.js
+++ src/utils/generateCoinList.js
@@ -1,26 +1,26 @@
 const { generate } = require('./RandomCoinGenerator.js');
 
 const generateCoinList = (moneyAmount) => {
-  const coinList = { ...dummyCoinList };
+  const coinList = new Map(dummyCoinList);
 
   while (moneyAmount) {
     const coinType = generate();
-    const isValidCoinType = moneyAmount - coinType >= 0;
 
+    const isValidCoinType = moneyAmount - coinType >= 0;
     if (!isValidCoinType) continue;
 
-    coinList[coinType] += 1;
+    coinList.set(coinType, coinList.get(coinType) + 1);
     moneyAmount -= coinType;
   }
 
   return coinList;
 };
 
-const dummyCoinList = {
-  500: 0,
-  100: 0,
-  50: 0,
-  10: 0,
-};
+const dummyCoinList = [
+  [500, 0],
+  [100, 0],
+  [50, 0],
+  [10, 0],
+];
 
 module.exports = generateCoinList;
